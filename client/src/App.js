51649dab336f98f09cc4662f9233306c75e3bfb7
melissastoenica <melissadorisa@gmail.com>
2023-05-17 16:32:46 +0300

    ProductsScreen and ProductCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
-import {ChakraProvider} from '@chakra-ui/react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import ProductsScreen from './screens/ProductsScreen';
 
 function App() {
   return (
-  <ChakraProvider>
-    <Router>
-      <NavBar />
-      <main>{/* <Routes></Routes> */}</main>
-    </Router>
-  </ChakraProvider>
+    <ChakraProvider>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path='/products' element={<ProductsScreen />}></Route>
+          </Routes>
+        </main>
+      </Router>
+    </ChakraProvider>
   );
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,65 +1,109 @@
-import { Box, Flex, HStack, Link, IconButton, Icon, Text, useDisclosure, Button, Stack, useColorModeValue, useColorMode} from '@chakra-ui/react';
-import {Link as ReactLink} from 'react-router-dom';
-import {HamburgerIcon, CloseIcon, MoonIcon, SunIcon} from '@chakra-ui/icons';
-import {RiStore2Line} from 'react-icons/ri';
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+  Button,
+  Stack,
+  useColorModeValue,
+  useColorMode,
+} from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { RiStore2Line } from 'react-icons/ri';
 
 const links = [
-    {linkName: 'Products', path: '/products'},
-    {linkName: 'ShoppingCart', path: '/cart'}
+  { linkName: 'Produse', path: '/products' },
+  { linkName: 'Coș de cumpărături', path: '/cart' },
 ];
 
-const NavLink = ({path, children}) => (
-    <Link as={ReactLink} to={path} px={2} py={2} rounded='md' _hover={{ textDecoration: 'none', bg: useColorModeValue('gray.200', 'gray.700')}}>{children}</Link>
+const NavLink = ({ path, children }) => (
+  <Link
+    as={ReactLink}
+    to={path}
+    px={2}
+    py={2}
+    rounded='md'
+    _hover={{ textDecoration: 'none', bg: useColorModeValue('gray.200', 'gray.700') }}
+  >
+    {children}
+  </Link>
 );
- 
+
 const NavBar = () => {
-    const {isOpen, onClose, onOpen} = useDisclosure();
-    const {colorMode, toggleColorMode} = useColorMode();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems='center' justifyContent='space-between'>
-            <IconButton 
-            size='md' 
-            icon={isOpen ? <CloseIcon/> : <HamburgerIcon />} 
-            display={{ md: 'none'}} 
-            onClick={isOpen ? onClose: onOpen} 
-            />
+      <Flex h={16} alignItems='center' justifyContent='space-between'>
+        <IconButton
+          size='md'
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
 
-            <HStack>
-                <Link as={ReactLink} to='/'>
-                    <Flex alignItems='center'>
-                        <Icon as={RiStore2Line} h={6} w={6} color='orange.400'/>
-                        <Text fontWeight='extrabold'> Magazin Online</Text>
-                    </Flex>
-                </Link>
-                <HStack as='nav' spacing={4} display={{ base: 'none', md: 'flex' }} >
-                    {links.map((link) => (
-                        <NavLink key={link.linkName} path={link.path}> {link.linkName} 
-                        </NavLink>
-                    ))}
-                </HStack>   
-            </HStack>
+        <HStack>
+          <Link as={ReactLink} to='/'>
             <Flex alignItems='center'>
-                <NavLink>
-                <Icon as={colorMode === 'light' ? MoonIcon : SunIcon} alignSelf='center'  onClick={()  => toggleColorMode()} />
-                </NavLink>
-                <Button as={ReactLink} to='/login' p={2} fontSize='sm' fontWeight={400} variant='link'>
-            Sign In 
-            </Button>
-            <Button as={ReactLink} to='/registration' m={2} display={{base: 'none', md: 'inline-flex'}} fontSize='sm' fontWeight={600} _hover={{bg: 'orange.400'}} bg='orange.500' color='white'>
-            Sign Up
-            </Button>
+              <Icon as={RiStore2Line} h={6} w={6} color='orange.400' />
+              <Text fontWeight='extrabold'> Magazin Online</Text>
             </Flex>
-        </Flex>
-        {isOpen ? <Box pb={4} display={{md: 'none'}}>
-            <Stack as='nav' spacing={4}>
+          </Link>
+          <HStack as='nav' spacing={4} display={{ base: 'none', md: 'flex' }}>
             {links.map((link) => (
-                        <NavLink key={link.linkName} path={link.path}> {link.linkName} 
-                        </NavLink>
-                    ))}
-                    <NavLink key='sign up' path='/registration'>Sign Up</NavLink>
-            </Stack>
-        </Box> : null}
+              <NavLink key={link.linkName} path={link.path}>
+                {' '}
+                {link.linkName}
+              </NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems='center'>
+          <NavLink>
+            <Icon
+              as={colorMode === 'light' ? MoonIcon : SunIcon}
+              alignSelf='center'
+              onClick={() => toggleColorMode()}
+            />
+          </NavLink>
+          <Button as={ReactLink} to='/login' p={2} fontSize='sm' fontWeight={400} variant='link'>
+            Conectează-te
+          </Button>
+          <Button
+            as={ReactLink}
+            to='/registration'
+            m={2}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize='sm'
+            fontWeight={600}
+            _hover={{ bg: 'orange.400' }}
+            bg='orange.500'
+            color='white'
+          >
+            Creează un cont{' '}
+          </Button>
+        </Flex>
+      </Flex>
+      {isOpen ? (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as='nav' spacing={4}>
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {' '}
+                {link.linkName}
+              </NavLink>
+            ))}
+            <NavLink key='sign up' path='/registration'>
+              Creează un cont
+            </NavLink>
+          </Stack>
+        </Box>
+      ) : null}
     </Box>
   );
 };
